62259109ae0fce982184b45f01c9c895e2c9a717
DUSERNAM3 <117265173+DUSERNAM3@users.noreply.github.com>
2024-04-11 23:38:20 +0300

    Initial commit

---
+++ OOP Lab 1/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Task3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Task3.Task work = new Task3.Task();
+        work.DoTask3();
+        Console.Read();
+    }
+}
